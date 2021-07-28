ba0d3f0a20544afe686aa24babbc6264c8ca5a3c
Jerome Laban <jerome.laban@nventive.com>
2021-07-28 14:33:25 +0000

    Repro for 56309

--- src/mono/sample/wasm/console/Program.cs
+++ src/mono/sample/wasm/console/Program.cs
@@ -3,16 +3,110 @@
 
 using System;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 public class Test
 {
     public static async Task<int> Main(string[] args)
     {
         await Task.Delay(1);
-        Console.WriteLine("Hello World!");
-        for (int i = 0; i < args.Length; i++) {
-            Console.WriteLine($"args[{i}] = {args[i]}");
+        TestEnumeratorListOfT();
+        TestForeachListOfT();
+        TestForListOfT();
+        TestForArray();
+
+        return 0;
+    }
+
+    private static void TestForArray()
+    {
+        var SUT = new List<object>();
+        SUT.AddRange(Enumerable.Range(1, 1000)
+            .Select(i => (object)null));
+        var SUT2 = SUT.ToArray();
+
+        var sw = Stopwatch.StartNew();
+
+        for (int i = 0; i < 5000; i++)
+        {
+            int count = 0;
+            foreach (var item in SUT2)
+            {
+                count++;
+            }
         }
-        return args.Length;
+
+        sw.Stop();
+        Console.WriteLine($"TestForArray: {sw.Elapsed}");
+    }
+
+
+    private static void TestForListOfT()
+    {
+        var SUT = new List<object>();
+        SUT.AddRange(Enumerable.Range(1, 1000)
+            .Select(i => (object)null));
+
+        var sw = Stopwatch.StartNew();
+
+        for (int j = 0; j < 5000; j++)
+        {
+            int count = 0;
+            for (int i = 0; i < SUT.Count; i++)
+            {
+                var r = SUT[i];
+                count++;
+            }
+        }
+
+        sw.Stop();
+        Console.WriteLine($"TestForListOfT: {sw.Elapsed}");
+    }
+
+    private static void TestForeachListOfT()
+    {
+        var SUT = new List<object>();
+        SUT.AddRange(Enumerable.Range(1, 1000)
+            .Select(i => (object)null));
+
+        var sw = Stopwatch.StartNew();
+
+        for (int i = 0; i < 5000; i++)
+        {
+            int count = 0;
+            foreach (var item in SUT)
+            {
+                count++;
+            }
+        }
+
+        sw.Stop();
+        Console.WriteLine($"TestForeachListOfT: {sw.Elapsed}");
+    }
+
+    private static void TestEnumeratorListOfT()
+    {
+        var SUT = new List<object>();
+        SUT.AddRange(Enumerable.Range(1, 1000)
+            .Select(i => (object)null));
+
+        var sw = Stopwatch.StartNew();
+
+        for (int i = 0; i < 5000; i++)
+        {
+            int count = 0;
+            var enumerator = SUT.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                var r = enumerator.Current;
+                count++;
+            }
+        }
+
+        sw.Stop();
+        Console.WriteLine($"TestEnumeratorListOfT: {sw.Elapsed}");
     }
 }
